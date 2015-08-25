99696a707b9776e908472e99c4b8bda9565b6220
ericox <emcox84@gmail.com>
2015-08-25 15:59:29 -0400

    moving chart config

--- app/public/js/common.js
+++ app/public/js/common.js
@@ -499,7 +499,82 @@ function resetConsole() {
 // 
 
 $(document).ready(function() {
-    qps_graph = new QueryGraph("Requests Per Second");
+    var chartConfig =  {
+    chart: {
+               renderTo: 'qps-graph',
+               type: 'spline',
+               events: {
+                   load: null /*function() {
+                             // set up the updating of the chart each second
+                             var series = this.series[0];
+                             setInterval(function() {
+                                 var x = (new Date()).getTime(), // current time
+                                 y = Math.random() / 5 + 0.8;
+                             series.addPoint([x, y]); //, true, true);
+                             }, 100);
+                         }*/
+               }
+           },
+    plotOptions: {
+                     series : {
+                                  lineWidth: 1,
+                                  marker: {
+                                      enabled: false
+                                  }
+                              }
+                 },
+    title: {
+               text: 'Live random data'
+           },
+    xAxis: {
+               type: 'datetime',
+               tickPixelInterval: 100
+           },
+    yAxis: {
+               title: {
+                          text: 'Requests Per Second'
+                      },
+               plotLines: [{
+                              value: 0,
+                              width: 1,
+                              color: '#808080'
+                          }]
+           },
+    tooltip: {
+                 formatter: function() {
+                                return '<b>'+ this.series.name +'</b><br/>'+
+                                    Highcharts.dateFormat('%Y-%m-%d %H:%M:%S', this.x) +'<br/>'+
+                                    Highcharts.numberFormat(this.y, 2);
+                            }
+             },
+    legend: {
+                enabled: false
+            },
+    exporting: {
+                   enabled: false
+               },
+    series: [{
+                name: 'Random data',
+                data:null
+                    /*
+                (function() {
+                    // generate an array of random data
+                    var data = [],
+                time = (new Date()).getTime(),
+                i;
+                for (i = -19; i <= 0; i++) {
+                    data.push({
+                        x: time + i * 1000,
+                        y: Math.random()
+                    });
+                }
+                return data;
+                })()
+                */
+            }]
+}
+
+    qps_graph = new QueryGraph("Requests Per Second", chartConfig);
 
     reloadDialMaxValue();
 
